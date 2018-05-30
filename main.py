e691c3938604d0387ce5c998d2b6cbe50cc90a66
Samusoidal <info@samusoidal.com>
2018-05-29 21:17:57 -0700

    Automatic Threads

--- main.py
+++ main.py
@@ -35,25 +35,30 @@ str_username = StringVar();
 str_password = StringVar();
 
 # METHODS
+
 def getNetworkIp():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     s.connect(('<broadcast>', 0))
     return s.getsockname()[0]
 
-def report():
-    rr = requests.put('http://' + str_server.get() + '/clients/report/' + str_chash.get(), auth=(str_username.get(), str_password.get()), json={ 'ip': getNetworkIp(), 'interval': str_cinterval.get() });
+# SHOWN
+def report(*args):
+
+    rr = requests.put('http://' + str_server.get() + '/clients/report/' + str_chash.get(), auth=(str_username.get(), str_password.get()), json={ 'ip': getNetworkIp(), 'interval': int(str_cinterval.get()) });
 
     if rr.status_code == 200:
         retrieve_settings();
 
 def retrieve_settings():
+
     rr = requests.get('http://' + str_server.get() + '/clients/report/' + str_chash.get(), auth=(str_username.get(), str_password.get()));
 
     if rr.status_code == 200:
-        str_cinterval = json.loads(rr.text)['interval'];
+        str_cinterval.set(json.loads(rr.text)['interval']);
 
 def load_settings():
+
     try:
         settings_file = open('settings.txt', 'r+');
     except IOError:
@@ -65,6 +70,7 @@ def load_settings():
         str_username.set(settings[1][:-1]);
         str_password.set(settings[2][:-1]);
         str_cname.set(settings[3][:-1]);
+        str_cinterval.set(str(int(settings[4][:-1])));
     except:
         print("Invalid, missing, or corrupted settings file, ignoring...");
 
@@ -72,9 +78,53 @@ def load_settings():
 
 def save_settings():
     settings_file = open('settings.txt', 'w+');
-    settings_file.write(str_server.get() + '\n' + str_username.get() + '\n' + str_password.get() + '\n' + str_cname.get() + '\n');
+    settings_file.write(str_server.get() + '\n' + str_username.get() + '\n' + str_password.get() + '\n' + str_cname.get() + '\n' + str_cinterval.get() + '\n');
+    settings_file.close();
+    reset_timer(int(str_cinterval.get())/1000);
+
+# HIDDEN
+def report_hidden(*args):
+
+    settings = load_settings_hidden();
+
+    rc = requests.get('http://' + settings[0][:-1] + '/clients/report/' + hashify(settings[3][:-1]), auth=(settings[1][:-1], settings[2][:-1]));
+    nd = json.loads(rc.text);
+
+    rr = requests.put('http://' + settings[0][:-1] + '/clients/report/' + hashify(settings[3][:-1]), auth=(settings[1][:-1], settings[2][:-1]), json={ 'ip': getNetworkIp(), 'interval': nd['interval'] });
+    print(rr.status_code);
+    if rr.status_code == 200:
+        retrieve_settings_hidden(settings);
+
+def retrieve_settings_hidden(settings):
+
+    rr = requests.get('http://' + settings[0][:-1] + '/clients/report/' + hashify(settings[3][:-1]), auth=(settings[1][:-1], settings[2][:-1]));
+
+    if rr.status_code == 200:
+        save_settings_hidden(settings, json.loads(rr.text));
+
+def load_settings_hidden():
+
+    try:
+        settings_file = open('settings.txt', 'r+');
+    except IOError:
+        settings_file = open('settings.txt', 'w+');
+
+    try:
+        settings = settings_file.readlines();
+        return settings
+    except:
+        print("Invalid, missing, or corrupted settings file, ignoring...");
+
     settings_file.close();
 
+def save_settings_hidden(settings, data):
+    settings_file = open('settings.txt', 'w+');
+    settings_file.write(settings[0][:-1] + '\n' + settings[1][:-1] + '\n' + settings[2][:-1] + '\n' + settings[3][:-1] + '\n' + str(data['interval']) + '\n');
+    settings_file.close();
+    reset_timer(int(data['interval'])/1000);
+
+
+# GUI
 def build_gui(root):
     try:
         dummy = Frame(root);
@@ -89,7 +139,6 @@ def build_gui(root):
     config_page_f1 = Frame(config_page);
     label_server = Label(config_page_f1, text="Server");
     input_server = Entry(config_page_f1, textvariable=str_server);
-    input_server.insert(0, "server:port");
     label_server.grid(row=1,column=1);
     input_server.grid(row=1,column=2);
 
@@ -142,8 +191,6 @@ def build_gui(root):
 
     #root.mainloop();
 
-load_settings();
-
 if __name__ == '__main__':
     import itertools, glob
 
@@ -183,11 +230,19 @@ if __name__ == '__main__':
     def report_dummy():
         print("Report");
 
+    def reset_timer(newdelay):
+        global timer
+        timer()
+        timer = call_repeatedly(newdelay, report_hidden);
+
     def bye(sysTrayIcon):
         if not gui_up:
             timer()
             mmw.destroy();
 
-    timer = call_repeatedly(5, report_dummy)
+    load_settings();
+
+    timer = call_repeatedly(5, report_hidden);
+
     systrayicon.SysTrayIcon(next(icons), hover_text, menu_options, on_quit=bye, default_menu_index=1);
     mmw.mainloop();
